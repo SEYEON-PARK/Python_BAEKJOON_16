4c523cab2eef5e569ea69e7a1af2e59f7604411d
SEYEON_PARK <fourleaf123@naver.com>
2021-02-03 01:05:02 +0900

    Python언어 BAEKJOON 10869번 문제 풀기!(3)

---
+++ PythonApplication50.py
@@ -0,0 +1,11 @@
+'''두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+(1 ≤ A, B ≤ 10,000)'''
+
+A=int(input("1 이상의 자연수를 입력하세요. : "))
+B=int(input("10,000 이하의 자연수를 입력하세요. : "))
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A%B)
