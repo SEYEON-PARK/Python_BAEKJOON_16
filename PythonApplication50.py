80d5719ba5416a052e68d09045c3333b5c090423
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-02-04 00:38:11 +0900

    Python언어 BAEKJOON 10869번 문제 풀기!(3)

--- PythonApplication50.py
+++ PythonApplication50.py
@@ -2,10 +2,10 @@
 (1 ≤ A, B ≤ 10,000)'''
 
 A=int(input("1 이상의 자연수를 입력하세요. : ")) #자연수 A 입력받기.
-B=int(input("10,000 이하의 자연수를 입력하세요. : "))
+B=int(input("10,000 이하의 자연수를 입력하세요. : ")) #자연수 B 입력받기.
 
 print(A+B)
 print(A-B)
 print(A*B)
 print(A/B)
-print(A%B) #
+print(A%B) #출력하기
